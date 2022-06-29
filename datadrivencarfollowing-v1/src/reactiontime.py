7d3feaf0ec3e590b2d4ae5f49537bf381263a2e8
neelschaudhari000 <neelschaudhari000@gmail.com>
2022-06-28 20:54:04 -0400

    reactiontime

---
+++ datadrivencarfollowing-v1/src/reactiontime.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import numpy as np
+
+
+
+def reactiontime1(i,df):
+    df["nextframeAcc"]=df.groupby(["L-F_Pair"],as_index=False)["v_Acc"].shift(-i)
+    df["nextframesvel"]=df.groupby(["L-F_Pair"],as_index=False)["v_Vel"].shift(-i)
+    df["nextframeposition"]=df.groupby(["L-F_Pair"],as_index=False)["Local_Y"].shift(-i)
+    return df
