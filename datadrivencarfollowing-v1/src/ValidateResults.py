1a58523a78c8944243fe73b9cc49e7ee7d13e9d2
neelschaudhari000 <neelschaudhari000@gmail.com>
2022-06-28 20:47:28 -0400

    rfproblem

---
+++ datadrivencarfollowing-v1/src/ValidateResults.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+
+class FileProcessing():
+    
+    def readInput(self, fileName):
+        '''
+        Read the input file into a dataframe. 
+        Input: File name for the file present in Data folder. 
+        Output: Dataframe name. 
+        '''
+        return df
